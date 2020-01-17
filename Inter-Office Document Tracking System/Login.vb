aed503f94d562c582a1d4697afbc8bb997811abe
MavonOcliaso <55120796+ocliasom@users.noreply.github.com>
2020-01-17 14:44:26 +0800

    office and AdminDashboard Form

--- Inter-Office Document Tracking System/Login.vb
+++ Inter-Office Document Tracking System/Login.vb
@@ -4,36 +4,50 @@ Public Class Login
 
     Dim connection As New MySqlConnection("datasource=localhost;port=3306;username=root;password=;database=iods")
     Dim Homepage As New Dashboard
+    Dim Adminpage As New AdminDashboard
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Sign_in.Click
         If Username.Text = "" Or Password.Text = "" Then
             MessageBox.Show("Please Fill in Empty Fields!")
         Else
             Dim cmd As MySqlCommand
-            Dim reader As MySqlDataReader
+            Dim rolereader As MySqlDataReader
             Try
                 connection.Open()
                 Dim query As String
                 query = "select role from user_login where username = '" & Username.Text & "' and password = '" & Password.Text & "' "
                 cmd = New MySqlCommand(query, connection)
-                reader = cmd.ExecuteReader()
+                rolereader = cmd.ExecuteReader()
 
                 Dim count As Integer
                 count = 0
-                While reader.Read
+                While rolereader.Read
                     count = count + 1
 
                 End While
                 If count = 1 Then
-                    Dim usertype = reader.GetString("role")
+                    Dim usertype = rolereader.GetString("role")
                     If usertype = "admin" Then
-                        Homepage.Show()
+                        Adminpage.Show()
                         Me.Hide()
                         connection.Close()
                     ElseIf usertype = "OFFICE" Then
-                        Form1.Show()
+                        Adminpage.Show()
                         Me.Hide()
                         connection.Close()
                     ElseIf usertype = "USER" Then
+                        connection.Close()
+                        connection.Open()
+
+                        Dim command2 As New MySqlCommand("SELECT `office` FROM `user` WHERE `username` = @username ", connection)
+                        Dim reader As MySqlDataReader
+                        command2.Parameters.Add("@username", MySqlDbType.VarChar).Value = Username.Text
+
+
+                        reader = command2.ExecuteReader()
+                        reader.Read()
+                        If reader.HasRows Then
+                            Homepage.Office = reader("office").ToString()
+                        End If
                         Homepage.Show()
                         Me.Hide()
                         connection.Close()
