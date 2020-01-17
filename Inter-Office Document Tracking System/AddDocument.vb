66ce7f78764134ca971e42bd9875cbbfc6e66a83
MavonOcliaso <55120796+ocliasom@users.noreply.github.com>
2020-01-17 15:38:14 +0800

    email proto

--- Inter-Office Document Tracking System/AddDocument.vb
+++ Inter-Office Document Tracking System/AddDocument.vb
@@ -1,4 +1,5 @@
 ﻿Imports MySql.Data.MySqlClient
+Imports System.Net.Mail
 Public Class AddDocument
 
     Dim connection As New MySqlConnection("datasource=localhost;port=3306;username=root;password=;database=iods")
@@ -59,24 +60,43 @@ Public Class AddDocument
                               MessageBoxButtons.YesNo)
 
         If (result = DialogResult.Yes) Then
-            Dim Addpage As New AddDocument
+            Try
+                Dim Addpage As New AddDocument
 
 
-            Dim command As New MySqlCommand("INSERT INTO `document` (current_office,date_received,description,doctype,source_office,status) values(@office,@date,@description,@doctype,@source,@status)", connection)
+                Dim command As New MySqlCommand("INSERT INTO `document` (current_office,date_received,description,doctype,source_office,status) values(@office,@date,@description,@doctype,@source,@status)", connection)
 
-            command.Parameters.AddWithValue("@office", Officebox.Text)
-            command.Parameters.AddWithValue("@date", Datebox.Text)
-            command.Parameters.AddWithValue("@description", TextBox3.Text)
-            command.Parameters.AddWithValue("@doctype", ComboBox1.SelectedValue)
-            command.Parameters.AddWithValue("@source", Officebox.Text)
-            command.Parameters.AddWithValue("@status", "pending")
+                command.Parameters.AddWithValue("@office", Officebox.Text)
+                command.Parameters.AddWithValue("@date", Datebox.Text)
+                command.Parameters.AddWithValue("@description", TextBox3.Text)
+                command.Parameters.AddWithValue("@doctype", ComboBox1.SelectedValue)
+                command.Parameters.AddWithValue("@source", Officebox.Text)
+                command.Parameters.AddWithValue("@status", "pending")
 
-            connection.Open()
-            command.ExecuteNonQuery()
-            connection.Close()
+                connection.Open()
+                command.ExecuteNonQuery()
+                connection.Close()
 
-        Else
+                Dim Smtp_server As New SmtpClient
+                Dim e_mail As New MailMessage
+                Smtp_server.UseDefaultCredentials = False
+                Smtp_server.Credentials = New Net.NetworkCredential("ocliasomavon@gmail.com", "Kiritetsuya1011")
+                Smtp_server.Port = 587
+                Smtp_server.EnableSsl = True
+                Smtp_server.Host = "smtp.gmail.com"
+                e_mail = New MailMessage
+                e_mail.From = New MailAddress("PUP@gmail.com")
+                e_mail.To.Add("iammavon@gmail.com")
+                e_mail.Subject = "Document Update"
+                e_mail.Body = "Your document has been added"
+                Smtp_server.Send(e_mail)
+                MessageBox.Show("Mail Sent")
 
+
+
+            Catch ex As Exception
+                MessageBox.Show(ex.Message)
+            End Try
         End If
     End Sub
 
